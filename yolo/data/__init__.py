22d8a3378b3a73e051ed83eaa5fa7a1867714a71
Muhammad Moin <79751534+MuhammadMoinFaisal@users.noreply.github.com>
2023-01-17 10:59:58 +0500

    Delete yolo directory

--- yolo/data/__init__.py
+++
@@ -1,6 +0,0 @@
-# Ultralytics YOLO 🚀, GPL-3.0 license
-
-from .base import BaseDataset
-from .build import build_classification_dataloader, build_dataloader
-from .dataset import ClassificationDataset, SemanticDataset, YOLODataset
-from .dataset_wrappers import MixAndRectDataset
--- yolo/v8/detect/__init__.py
+++
@@ -1,5 +0,0 @@
-# Ultralytics YOLO 🚀, GPL-3.0 license
-
-from .predict import DetectionPredictor, predict
-from .train import DetectionTrainer, train
-from .val import DetectionValidator, val
